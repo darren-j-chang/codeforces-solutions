cb97cb4a827cc42791cba41df1531fd700e57301
Darren Chang <darrenchangla@gmail.com>
2025-08-07 20:49:14 -0700

    800/71A-WayTooLongWords

---
+++ 800/71A-WayTooLongWords.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Darren Chang on 8/7/25.
+//
+#include <iostream>
+#include <string>
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        std::string word;
+        std::cin >> word;
+
+        if (word.length() > 10) {
+            std::cout << word[0] << word.length()-2 << word[word.length()-1] << std::endl;
+        } else {
+            std::cout << word << std::endl;
+        }
+    }
+    return 0;
+}
