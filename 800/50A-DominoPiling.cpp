51129c4ab2879be321d9a454009dcd8c0d966f9c
Darren Chang <darrenchangla@gmail.com>
2025-08-20 22:04:49 -0700

    800/50A-DominoPiling.cpp

---
+++ 800/50A-DominoPiling.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Darren Chang on 8/20/25.
+//
+#include <iostream>
+
+int main() {
+	int M, N;
+	std::cin >> M >> N;
+
+	int result = (M * N) / 2;
+
+	std::cout << result << std::endl;
+
+	return 0;
+}
